e23df85df03587336c82cd5328660114192e27db
HHcasper <bhm494@myy.haaga-helia.fi>
2026-09-15 08:46:47 +0300

    Add book list page

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -1,13 +1,26 @@
 package backend.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import backend.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookRepository repository;
+
     @GetMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
