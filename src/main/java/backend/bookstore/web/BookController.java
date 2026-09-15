e9b2c6fab4b801d5d6d3851d2a30b09427251f68
HHcasper <bhm494@myy.haaga-helia.fi>
2026-09-15 08:55:56 +0300

    Delete ja Add

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import backend.bookstore.domain.Book;
 import backend.bookstore.domain.BookRepository;
 
 @Controller
@@ -22,5 +25,23 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/savebook")
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        repository.deleteById(bookId);
+        return "redirect:/booklist";
     }
 }
